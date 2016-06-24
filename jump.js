dc7b450e136c64f05cac20f8e9e3cceb92b59e13
Atsushi Watanabe <atsushi.w@ieee.org>
2016-06-25 01:15:02 +0900

    fixes too large scroll

--- jump.js
+++ jump.js
@@ -11,41 +11,67 @@ $().ready( function(){
 	var slide = false;
 	$('#slidelike').click(function (){
 		if(slide == false){
-			$("h2, h3").css("margin-top", "20em");
+			$("h2").css("margin-top", "1024px");
+			$("h3").each(function(){
+				if($(this).prev()[0]){
+					$(this).css("margin-top", "1024px");
+				}
+			});
+			//$("h2, h3").css("margin-top", "1024px");
 			$("body").css("overflow", "hidden");
+			$(".width100").css("max-width", "80%");
+			$(".width75").css("max-width", "60%");
+			$(".width50").css("max-width", "40%");
 			slide = true;
 		}else{
 			$("h2").css("margin-top", "40px");
 			$("h3").css("margin-top", "20px");
 			$("body").css("overflow", "auto");
+			$(".width100").css("max-width", "100%");
+			$(".width75").css("max-width", "75%");
+			$(".width50").css("max-width", "50%");
 			slide = false;
 		}
 	});
 	$('#next').click(function (){
 		var top = $("body, html").scrollTop();
 		var scroll_h = 0;
+		var topoff = 0;
 		$("h2, h3, article>ul>li, article>ol>li").each(function(){
 			var h = $(this).offset().top;
 			if(h > top + 5)
 			{
 				scroll_h = h;
+				topoff = h;
+				if($(this).is("h2, h3")){
+					if(slide == true){
+						topoff -= 1024;
+					}else{
+						topoff -= 40;
+					}
+				}
 				return false;
 			}
 		});
-		if(scroll_h > top + $(window).height())
+		if(topoff > top + $(window).height())
 		{
-			$("img, div.source_code").each(function(){
+			$("img, div.source_code, dl").each(function(){
 				if($(this).next().length && !$(this).next().is("h2, h3"))
 				{
 					var h = $(this).offset().top + $(this).outerHeight() + 1;
-					if(h > scroll_h - 100) return false;
+					if(h > scroll_h - 64) return false;
 					if(h > top + 64)
 					{
 						scroll_h = h;
+						topoff = h;
 						return false;
 					}
 				}
 			});
+			if(topoff > top + $(window).height())
+			{
+				scroll_h = top + $(window).height() - 100;
+			}
 		}
 
 		if(scroll_h != 0)
